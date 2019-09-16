d2b0d9a1daf06a4180816d5af995a9c8f7eb3a6c
Tarık Taha Uygun <ttahauygun@gmail.com>
2019-09-16 11:14:45 +0300

    Initial Commit

---
+++ VideoPlayerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerExample
+//
+//  Created by rapsodo-mobil-5 on 16.09.2019.
+//  Copyright © 2019 rapsodo-mobil-5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
